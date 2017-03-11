c47a7a06f46d2d35301b5b0085efe49d2b578125
mykytenkobohdan <mykytenko.bohdan@gmail.com>
2017-03-11 15:17:16 +0200

    init translate

--- src/app/components/header/header.directive.js
+++ src/app/components/header/header.directive.js
@@ -7,8 +7,27 @@ export function HeaderDirective() {
     templateUrl: 'app/components/header/header.html',
     controller: HeaderController,
     controllerAs: 'header',
-    bindToController: true
+    bindToController: true,
+    link: function () {
+    }
   };
 }
 
-class HeaderController {}
+function HeaderController($rootScope) {
+  'ngInject';
+  let vm = this;
+
+  $rootScope.language = 'en';
+  vm.language = $rootScope.language;
+  vm.toggle = false;
+
+  vm.selectLanguage = function (lang) {
+    vm.language = lang;
+    $rootScope.language = lang;
+    vm.toggle = false;
+  };
+
+  vm.activateDropdown = function () {
+    return vm.toggle = vm.toggle === false ? true : false;
+  };
+}
