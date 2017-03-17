c79256966bc6f7322cc9b8526ceb2b2491a31ec1
mykytenkobohdan <mykytenko.bohdan@gmail.com>
2017-03-17 16:08:25 +0200

    form submit

--- src/app/views/contact/contact.controller.js
+++ src/app/views/contact/contact.controller.js
@@ -1,3 +1,15 @@
 export class ContactController {
-  constructor() {}
+  constructor($http, $scope) {
+    'ngInject';
+    this.scope = $scope;
+
+  }
+
+  sendMail(name, email, text) {
+    console.log(name, email, text);
+
+    this.scope.contact.name = angular.copy('');
+    this.scope.contact.email = angular.copy('');
+    this.scope.contact.text = angular.copy('');
+  }
 }
