b0ff50587aa3e026c827ac751718cb0784bdccb5
mykytenkobohdan <mykytenko.bohdan@gmail.com>
2017-03-30 16:57:46 +0300

    skills directive

--- src/app/views/skills/skills.directive.js
+++ src/app/views/skills/skills.directive.js
@@ -1,14 +1,33 @@
 export function SkillsDirective() {
   'ngInject';
+
   return {
     restrict: 'E',
     replace: true,
-    templateUrl: 'app/views/skills/skills.html',
-    link: SkillsFunction
+    link: SkillsGallery
   }
 }
 
-function SkillsFunction() {
+function SkillsGallery(scope, element) {
   'ngInject';
 
+  element.find('#carousel').Cloud9Carousel({
+    buttonLeft: angular.element(".carousel_left"),
+    buttonRight: angular.element(".carousel_right"),
+    autoPlay: 1,
+    mirror: {
+      gap: 12,
+      height: 0.3,
+      opacity: 0.3
+    },
+    bringToFront: true,
+    onRendered: rendered
+  });
+}
+
+function rendered(carousel) {
+  let fade = Math.cos((carousel.floatIndex() % 1) * 2 * Math.PI);
+
+  angular.element('.carousel_title').text(carousel.nearestItem().element.alt);
+  angular.element('.carousel_title').css('opacity', 0.5 + (0.5 * fade));
 }
