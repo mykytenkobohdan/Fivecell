be79517f047a7bdb0673fa4d63433231e84156d3
Mykytenko Bohdan <mykytenko.bohdan@gmail.com>
2017-04-07 16:10:39 +0300

    finished resize carousel

--- src/app/views/skills/skills.directive.js
+++ src/app/views/skills/skills.directive.js
@@ -11,32 +11,50 @@ export function SkillsDirective() {
 function SkillsGallery(scope, element) {
     'ngInject';
 
-    let windowResizeVal;
+    let windowResizeVal = element.find('.container').width();
+
+    activateCarousel(windowResizeVal);
 
     scope.$on('$root.resizeWidth', function (event, value) {
-        // console.log('$$Width resize in directive', value);
-        windowResizeVal = value;
+        deactivateCarousel();
+
+        if (value >= 1200) {
+            activateCarousel(1200);
+        }
+
+        if (value >= 992 && value < 1200) {
+            activateCarousel(992)
+        }
+
+        if (value >= 768 && value < 992) {
+            activateCarousel(768)
+        }
+
         scope.$apply();
     });
 
-    console.log('$$Width resize in directive', windowResizeVal);
+    function activateCarousel(width) {
+        return element.find('#carousel').Cloud9Carousel({
+            buttonLeft: angular.element(".carousel_left"),
+            buttonRight: angular.element(".carousel_right"),
+            autoPlay: 1,
+            yOrigin: 90,
+            yRadius: innerHeight / 8,
+            xRadius: width / 2,
+            mirror: {
+                gap: 12,
+                height: 0.3,
+                opacity: 0.3
+            },
+            bringToFront: true,
+            onLoaded: loaded,
+            onRendered: rendered
+        });
+    }
 
-    element.find('#carousel').Cloud9Carousel({
-        buttonLeft: angular.element(".carousel_left"),
-        buttonRight: angular.element(".carousel_right"),
-        autoPlay: 1,
-        yOrigin: 90,
-        yRadius: innerHeight / 8,
-        xRadius: innerWidth / 3.2,
-        mirror: {
-            gap: 12,
-            height: 0.3,
-            opacity: 0.3
-        },
-        bringToFront: true,
-        onLoaded: loaded,
-        onRendered: rendered
-    });
+    function deactivateCarousel() {
+       return element.find('#carousel').data("carousel").deactivate();
+    }
 
     function rendered(carousel) {
         let fade = Math.cos((carousel.floatIndex() % 1) * 2 * Math.PI);
@@ -46,7 +64,7 @@ function SkillsGallery(scope, element) {
     }
 
     function loaded(carousel) {
-        if (angular.element('body').width() <= 767) {
+        if (windowResizeVal <= 767) {
             carousel.deactivate();
         }
     }
