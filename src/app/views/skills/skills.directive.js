93a5e78bbc128157686e039579842c834916a083
Mykytenko Bohdan <mykytenko.bohdan@gmail.com>
2017-04-07 17:55:31 +0300

    finished resize carousel

--- src/app/views/skills/skills.directive.js
+++ src/app/views/skills/skills.directive.js
@@ -53,11 +53,12 @@ function SkillsGallery(scope, element) {
     }
 
     function deactivateCarousel() {
-       return element.find('#carousel').data("carousel").deactivate();
+        return element.find('#carousel').data("carousel").deactivate();
     }
 
     function rendered(carousel) {
         let fade = Math.cos((carousel.floatIndex() % 1) * 2 * Math.PI);
+        console.log(carousel.nearestItem());
 
         element.find('.carousel_title').text(carousel.nearestItem().element.alt);
         element.find('.carousel_title').css('opacity', 0.5 + (0.5 * fade));
@@ -68,5 +69,52 @@ function SkillsGallery(scope, element) {
             carousel.deactivate();
         }
     }
+
+//    canvas test
+    angular.element('.test').click(function () {
+        const width = parseFloat(angular.element(this).css('width')) * 2;
+        const height = parseFloat(angular.element(this).css('height')) * 2;
+
+        angular.element('.test-wrap #skills').remove();
+        angular.element(this).after('<canvas id="skills" height="' + height + '" width="' + width + '"></canvas>');
+
+
+        let canvas = angular.element('.test-wrap canvas').get(0);
+        let context = canvas.getContext('2d');
+        const x = width / 2;
+        const y = height / 2;
+
+        const radius = Math.ceil(Math.sqrt(Math.pow((width / 2), 2) + Math.pow((height / 2), 2)) / 2) + 6;
+
+        let endPercent = 101;
+        let startPercent = 0;
+        const circ = Math.PI * 2;
+        const quart = Math.PI / 2;
+
+        context.lineWidth = 12;
+        context.strokeStyle = '#ad2323';
+        context.shadowOffsetX = 0;
+        context.shadowOffsetY = 0;
+        context.shadowBlur = 10;
+        context.shadowColor = '#656565';
+
+        renderScene();
+
+        function renderScene(current) {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.beginPath();
+
+            context.arc(x, y, radius, -(quart), ((circ) * current) - quart, false);
+            context.stroke();
+            startPercent++;
+
+            if (startPercent < endPercent) {
+                requestAnimationFrame(function () {
+                    renderScene(startPercent / 100)
+                });
+            }
+        }
+    });
+
 }
 
